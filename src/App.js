66999a8266449864a38d4e29583d6e351dc48948
Noevenzahn <info.artuofficial@gmail.com>
2021-04-24 20:28:08 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [count, setCount] = useState(0);
+
+    const random = () => {
+        setCount(Math.floor(Math.random() * 99))
+    };
+
+    return (
+        <div className="counter">
+            <p className="number">{count}</p>
+            <div className="button-group">
+                <button 
+                    className="button adjust-button" 
+                    onClick={() => setCount(count - 1)}>
+                -</button>
+                <button 
+                    className="button" 
+                    onClick={random}>
+                random</button>
+                <button 
+                    className="button adjust-button" 
+                    onClick={() => setCount(count + 1)}>
+                +</button>
+            </div>
+            <button 
+                className="button secondary-button" 
+                onClick={() => setCount(0)}>
+            clear</button>
+        </div>
+    )
+
+
 }
 
+
 export default App;
+
+// random number 
+// + 10, +100, +1000 usw
